253d4849a70da15c348c338570b84a32c80254eb
kaileh57 <kellenhe@hotmail.com>
2024-06-29 16:19:45 -0700

    started work on start sequence

--- tilemaps/start.gd
+++ tilemaps/start.gd
@@ -1,16 +1,29 @@
 extends Node3D
 @onready var light_1 = $Light1
 @onready var light_2 = $Light2
+@onready var countdown = $Countdown
+@onready var txt = $Countdown/Txt
 
 
 
 
 func _ready():
+	await get_tree().create_timer(5.0).timeout
 	go()
 
 func go():
+	countdown.show
+	light_1.show
+	light_2.show
+	light_1.mesh.material.emission = Color.DARK_RED
+	
+	await get_tree().create_timer(1.0).timeout
 	light_1.mesh.material.emission = Color.RED
-	await get_tree().create_timer(2.0).timeout
+	txt.text = "3"
+	
+	await get_tree().create_timer(1.0).timeout
 	light_1.mesh.material.emission = Color.DARK_ORANGE
-	await get_tree().create_timer(2.0).timeout
+	txt.text = "2"
+	
+	await get_tree().create_timer(1.0).timeout
 	light_1.mesh.material.emission = Color.FOREST_GREEN
